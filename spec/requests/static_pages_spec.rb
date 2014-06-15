bc99f219b35da7d1eeedbfd50b47d17d5c5ac9bf
vmobile7 <t4.facebook.dev@gmail.com>
2014-06-15 13:50:19 +0000

    add help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,6 +44,20 @@ describe "StaticPages" do
 
   end
 
+  describe "Contact page" do
+      it "should have the content 'Contact'" do 
+          visit '/static_pages/contact'
+          expect(page).to have_content('Contact')
+      end
+
+    it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+
+  end
+
 
 
 end
