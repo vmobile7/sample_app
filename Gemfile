9e7aec070cd4e9202686efb629287fef6795006b
vmobile7 <t4.facebook.dev@gmail.com>
2014-06-20 13:33:51 +0000

    add

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ group :development, :test do
     gem "pry-debugger"
     gem "pry-doc"
     gem "rdefs"
+    gem "guard-rspec", "2.5.0"
+    gem "spork-rails", "4.0.0"
+    gem "guard-spork", "1.5.0"
+    gem "childprocess", "0.5.3"
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"  }
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
         visit '/static_pages/home'
@@ -9,9 +11,14 @@ describe "StaticPages" do
     end
 
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
         visit '/static_pages/home'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+        expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custome page title " do
+        visit '/static_pages/home'
+        expect(page).not_to have_title("Home")
     end
 
   end
@@ -24,7 +31,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
         visit '/static_pages/help'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+        expect(page).to have_title("#{base_title} | Help")
     end
 
 
@@ -38,7 +45,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
         visit '/static_pages/about'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+        expect(page).to have_title("#{base_title} | About Us")
     end
 
 
@@ -52,7 +59,7 @@ describe "StaticPages" do
 
     it "should have the title 'Contact'" do
         visit '/static_pages/contact'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+        expect(page).to have_title("#{base_title} | Contact")
     end
 
 
